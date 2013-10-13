ab3073926fb579b71ee20168190eb194cc6db721
kasun04 <kasun147@gmail.com>
2013-10-13 21:40:38 +0530

    Adding Saxon based CBR support

--- modules/mediation-engine/src/main/java/org/siddhiesb/engine/util/XPathEvaluator.java
+++ modules/mediation-engine/src/main/java/org/siddhiesb/engine/util/XPathEvaluator.java
@@ -1,6 +1,6 @@
 package org.siddhiesb.engine.util;
 
-import org.apache.synapse.util.xmlstreamingxpath.XMLStreamingXPath;
+//import org.apache.synapse.util.xmlstreamingxpath.XMLStreamingXPath;
 import org.siddhiesb.common.api.PassThruContext;
 import org.siddhiesb.transport.passthru.PassThroughConstants;
 import org.siddhiesb.transport.passthru.Pipe;
@@ -19,20 +19,58 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+import javax.xml.transform.stream.StreamSource;
+
+import net.sf.saxon.s9api.DocumentBuilder;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XdmValue;
+
 
 @SiddhiExtension(namespace = "siddhiESB", function = "evalXPath")
 public class XPathEvaluator extends FunctionExecutor {
 
     private ExpressionExecutor expressionExecutor;
-    private XMLStreamingXPath xmlStreamingXPath;
+    //private XMLStreamingXPath xmlStreamingXPath;
+
+
+    /*Saxon*/
+    private XPathSelector selector;
+    private DocumentBuilder documentBuilder;
 
     @Override
     public void init(Attribute.Type[] types, SiddhiContext siddhiContext) {
         /* ToDo :NPEs FIx this */
         String xpathExpr = ((ConstantExpressionExecutor) attributeExpressionExecutors.get(0)).execute(null).toString();
-        xmlStreamingXPath = new XMLStreamingXPath();
 
-        xmlStreamingXPath.setXpathQuery(xpathExpr);
+        /*XML Stream XPath*/
+        //xmlStreamingXPath = new XMLStreamingXPath();
+        //xmlStreamingXPath.setXpathQuery(xpathExpr);
+
+
+        /*Saxon*/
+        Processor proc = new Processor(false);
+        XPathCompiler xpath = proc.newXPathCompiler();
+
+        documentBuilder = proc.newDocumentBuilder();
+        documentBuilder.setDTDValidation(false);
+
+        try {
+            selector = xpath.compile(xpathExpr).load();
+        } catch (SaxonApiException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
@@ -41,7 +79,8 @@ public class XPathEvaluator extends FunctionExecutor {
         if (eventObj instanceof InEvent) {
             if (((InEvent) eventObj).getData0() instanceof PassThruContext) {
                 PassThruContext passThruContext = (PassThruContext) ((InEvent) eventObj).getData0();
-                resultVal = evaluateXpath(passThruContext).toString();
+                /*Calling Saxon XPath*/
+                resultVal = evaluateSaxonXpath(passThruContext).toString();
             }
         }
         return resultVal;
@@ -62,7 +101,7 @@ public class XPathEvaluator extends FunctionExecutor {
                 try {
                     bufferedInputStream.reset();
                     bufferedInputStream.mark(0);
-                    passThruContext.setProperty(org.apache.synapse.transport.passthru.PassThroughConstants.BUFFERED_INPUT_STREAM, bufferedInputStream);
+                    passThruContext.setProperty(PassThroughConstants.BUFFERED_INPUT_STREAM, bufferedInputStream);
                 } catch (Exception e) {
                 }
                 return null;
@@ -74,21 +113,66 @@ public class XPathEvaluator extends FunctionExecutor {
             }
             pipe.setRawSerializationComplete(true);
 
-            resultStr= (String) xmlStreamingXPath.getValueOf(bufferedInputStream);
+            //resultStr= (String) xmlStreamingXPath.getValueOf(bufferedInputStream);
 
-            /*Call Streaming XPath HERE*/
         }
         return resultStr;
     }
 
+
+    private Object evaluateSaxonXpath(PassThruContext passThruContext) {
+
+        String resultStr = "";
+        Pipe pipe = (Pipe) passThruContext.getProperty(PassThroughConstants.PASS_THROUGH_PIPE);
+        if (pipe != null) {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(pipe.getInputStream());
+            bufferedInputStream.mark(128 * 1024);
+            OutputStream resetOutStream = pipe.resetOutputStream();
+
+            ReadableByteChannel inputChannel = Channels.newChannel(bufferedInputStream);
+            WritableByteChannel outputChannel = Channels.newChannel(resetOutStream);
+            if (!fastChannelCopy(inputChannel, outputChannel)) {
+                try {
+                    bufferedInputStream.reset();
+                    bufferedInputStream.mark(0);
+                    passThruContext.setProperty(PassThroughConstants.BUFFERED_INPUT_STREAM, bufferedInputStream);
+                } catch (Exception e) {
+                }
+                return null;
+            }
+            try {
+                bufferedInputStream.reset();
+                bufferedInputStream.mark(0);
+            } catch (Exception e) {
+            }
+            pipe.setRawSerializationComplete(true);
+
+            XdmValue xdmValue = null;
+            try {
+                XdmNode doc = documentBuilder.build(new StreamSource(bufferedInputStream));
+                selector.setContextItem(doc);
+
+                // Evaluate the expression.
+                xdmValue = selector.evaluate();
+            } catch (SaxonApiException e) {
+                e.printStackTrace();
+            }
+            if (xdmValue.size() > 0) {
+                resultStr = xdmValue.itemAt(0).getStringValue();
+            }
+        }
+        return resultStr;
+    }
+
+
     public boolean fastChannelCopy(final ReadableByteChannel src, final WritableByteChannel dest) {
-        final ByteBuffer buffer = ByteBuffer.allocateDirect(16*1024);
-        int i =1;
-        int size = 1024*8;
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+        int i = 1;
+        int size = 1024 * 8;
         try {
             while (src.read(buffer) != -1) {
-                int remains =size-(8*1024*i);
-                if(remains<0){//remains zero..
+                int remains = size - (8 * 1024 * i);
+                if (remains < 0) {//remains zero..
                     return false;
                 }
 
@@ -111,7 +195,6 @@ public class XPathEvaluator extends FunctionExecutor {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return true;
     }
 
@@ -152,7 +235,8 @@ public class XPathEvaluator extends FunctionExecutor {
                         "         </request>\n" +
                         "      </getQuote>";
 
-        InputStream inputStream = new ByteArrayInputStream(exampleXML.getBytes());
+        /*XML Stream XPath*/
+        /*InputStream inputStream = new ByteArrayInputStream(exampleXML.getBytes());
         String xpathString = "/getQuote/request/symbol";
         XMLStreamingXPath xmlStreamingXPath = new XMLStreamingXPath();
         xmlStreamingXPath.setXpathQuery(xpathString);
@@ -164,7 +248,49 @@ public class XPathEvaluator extends FunctionExecutor {
         System.out.println("Val " + val1);
         System.out.println("Val " + val2);
         System.out.println("Val " + val3);
+        */
 
+        String xpathSearch = "/getQuote/request/symbol";
+        Processor proc = new Processor(false);
+        XPathCompiler xpath = proc.newXPathCompiler();
+
+        Map<String, String> nsMap = new HashMap<String, String>();
+        Pattern nsPat = Pattern.compile("xmlns:(\\w+)=\"([^\"]+)\"");
+        Matcher nsMat = nsPat.matcher(exampleXML);
+        while (nsMat.find()) {
+            String ns = nsMat.group(1);
+            String url = nsMat.group(2);
+            nsMap.put(ns, url);
+        }
+        for (String ns : nsMap.keySet()) {
+            xpath.declareNamespace(ns, nsMap.get(ns));
+        }
+
+
+        try {
+            DocumentBuilder builder = proc.newDocumentBuilder();
+            builder.setDTDValidation(false);
+
+            // Load the XML document.
+            // Note that builder.build() can also take a File arg.
+            StringReader reader = new StringReader(exampleXML);
+            XdmNode doc = builder.build(new StreamSource(reader));
+
+            // Select all <book> nodes.
+            // XPath syntax: http://www.w3schools.com/xpath/xpath_syntax.asp
+            XPathSelector selector = xpath.compile(xpathSearch).load();
+            selector.setContextItem(doc);
+
+            // Evaluate the expression.
+            XdmValue xdmValue = selector.evaluate();
+
+            String val = xdmValue.itemAt(0).getStringValue();
+            System.out.println(" val " + val);
+
+
+        } catch (SaxonApiException e) {
+            e.printStackTrace();
+        }
 
     }
 }
